d691d19cf21eeeb48191005e74ba02ae073c31a4
velen2077 <velen2077@proton.me>
2024-11-20 17:25:28 +0000

    Updated kde system module.

--- modules/nixos/kde/default.nix
+++ modules/nixos/kde/default.nix
@@ -12,7 +12,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-    networking.networkmanager.enable = true;
+    # Enable the KDE Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
+    services.displayManager.sddm.enableHidpi = true;
+    # services.displayManager.sddm.theme = "breeze-user";
 
     environment.sessionVariables = {
       LIBVA_DRIVER_NAME = "nvidia";
@@ -21,28 +25,38 @@ in {
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       NVD_BACKEND = "direct";
       MOZ_DISABLE_RDD_SANDBOX = 1;
-      NIXOS_OZONE_WL = 1;
+      NIXOS_OZONE_WL = "1";
+      QT_LOGGING_RULES = "*.debug=false";
+      GTK_USE_PORTAL = 1;
+      GDK_DEBUG = "portals";
     };
+
+    qt = {
+      enable = true;
+      platformTheme = "kde";
+    };
+
+    environment.systemPackages = with pkgs.kdePackages; [
+      sddm-kcm
+      audiocd-kio
+      skanpage
+      isoimagewriter
+      krdc
+      konversation
+      breeze-icons
+      discover
+      partitionmanager
+      kclock
+      #pkgs.exfatprogs
+      #pkgs.leoPkgs.sddm-theme-breeze-user
+    ];
 
     services.desktopManager.plasma6.enable = true;
-    services.displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
+    xdg.icons.enable = true;
+
+    xdg.portal = {
+      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+      xdgOpenUsePortal = true;
     };
-
-    environment.plasma6.excludePackages = with pkgs.kdePackages; [
-      plasma-browser-integration
-      khelpcenter
-      krdp
-    ];
-
-    environment.systemPackages = with pkgs; [
-      nvidia-vaapi-driver
-      kdePackages.sddm-kcm
-      (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
-        [General]
-      '')
-      networkmanager-openvpn
-    ];
   };
 }
