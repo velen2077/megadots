705398f619bd5b41bb25f3bec2f58fc164bf5fd3
velen2077 <velen2077@proton.me>
2024-11-21 11:08:38 +0000

    Updated comments and readme.

--- flake.nix
+++ flake.nix
@@ -1,54 +1,50 @@
 {
+  # Welcome to megadots by velen2077. Find out more at
+  # https://github.com/velen2077/megadots
   description = "megadots by velen2077.";
 
+  # Substituters currently used for Cachy kernel
+  # so that it doesn't need to be compiled everytime.
   nixConfig = {
     extra-substituters = [
-      #"https://cache.m7.rs"
       "https://chaotic-nyx.cachix.org/"
-      #"https://nix-community.cachix.org"
-      #"https://nix-gaming.cachix.org"
     ];
     extra-trusted-public-keys = [
-      #"cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-      #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
 
+  # Inputs for my megadots configuration. These are used throughout
+  # NixOS and Home Manager and are always evolving as I add more
+  # functionality to my config.
   inputs = {
     # Nix ecosystem.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     # Common hardware.
     hardware.url = "github:nixos/nixos-hardware";
-
     # Chaotic inputs for CachyOS and Zen kernels.
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
     # Nix color scheming.
     nix-colors.url = "github:misterio77/nix-colors";
-
     # Home Manager.
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     # KDE Plasma manager.
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-
     # Secrets management with SOPS and Nix.
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Firefox addons to support my Firefox module.
+    # Firefox addons to support my Firefox
+    # Home Manager module. These allow installation
+    # of Firefox Extensions such as uBlock Origin.
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -73,7 +69,6 @@
         nixpkgs.lib.nixosSystem {
           inherit specialArgs;
           system = "x86_64-linux";
-
           modules = [
             ./hosts/${hostname}/configuration.nix
             chaotic.nixosModules.default
