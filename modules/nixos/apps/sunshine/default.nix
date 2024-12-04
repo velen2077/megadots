c7a7686259e618f7915433e1989b6591f11a5e69
velen2077 <velen2077@proton.me>
2024-12-04 16:02:35 +0000

    Updated sunshine module.

--- modules/nixos/apps/sunshine/default.nix
+++ modules/nixos/apps/sunshine/default.nix
@@ -12,33 +12,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-    networking.firewall.allowedTCPPortRanges = [
-      {
-        from = 47984;
-        to = 48010;
-      }
-    ];
-    networking.firewall.allowedUDPPortRanges = [
-      {
-        from = 47998;
-        to = 48010;
-      }
-    ];
-    security.wrappers.sunshine = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_sys_admin+p";
-      source = "${pkgs.sunshine}/bin/sunshine";
-    };
-    systemd.user.services.sunshine = {
-      description = "Sunshine self-hosted game stream host for Moonlight";
-      startLimitBurst = 5;
-      startLimitIntervalSec = 500;
-      serviceConfig = {
-        ExecStart = "${config.security.wrapperDir}/sunshine";
-        Restart = "on-failure";
-        RestartSec = "5s";
-      };
+    services.sunshine = {
+      enable = true;
+      autoStart = true;
+      capSysAdmin = true;
+      openFirewall = true;
     };
   };
 }
