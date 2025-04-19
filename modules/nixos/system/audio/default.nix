38365be591165697e3309fd2306df10aad13c640
velen2077 <velen2077@proton.me>
2025-04-19 22:34:18 +0100

    Added audio module.

---
+++ modules/nixos/system/audio/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.megadots.system.audio;
+in {
+  options.megadots.system.audio = {
+    enable = mkEnableOption "Enable audio for the system.";
+  };
+  config = mkIf cfg.enable {
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      pamixer
+      pavucontrol
+    ];
+  };
+}
