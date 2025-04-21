5c6cac5e759c1e877b5e9223263bb74b4c3049b3
velen2077 <velen2077@proton.me>
2025-04-21 13:22:00 +0100

    Updated home configs.

---
+++ home/velen2077/features/cli/fish/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    #./tide.nix
+    #./bindings.nix
+  ];
+  home.packages = [pkgs.bash-completion];
+  programs.fish = {
+    enable = true;
+    shellAbbrs = rec {
+      jqless = "jq -C | less -r";
+
+      n = "nix";
+      nd = "nix develop -c $SHELL";
+      ns = "nix shell";
+      nsn = "nix shell nixpkgs#";
+      nb = "nix build";
+      nbn = "nix build nixpkgs#";
+      nf = "nix flake";
+
+      nr = "nixos-rebuild --flake .";
+      nrs = "nixos-rebuild --flake . switch";
+      snr = "sudo nixos-rebuild --flake .";
+      snrs = "sudo nixos-rebuild --flake . switch";
+      hm = "home-manager --flake .";
+      hms = "home-manager --flake . switch";
+    };
+    shellAliases = {
+      # Clear screen and scrollback
+      clear = "printf '\\033[2J\\033[3J\\033[1;1H'";
+    };
+    functions = {
+      # Disable greeting
+      fish_greeting = "";
+    };
+  };
+}
---
+++ home/velen2077/global/default.nix
@@ -0,0 +1,43 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  config,
+  outputs,
+  ...
+}: {
+  imports =
+    [
+      ../features/cli/fish
+    ]
+    ++ (builtins.attrValues outputs.homeManagerModules);
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      warn-dirty = false;
+    };
+  };
+
+  systemd.user.startServices = "sd-switch";
+
+  programs = {
+    home-manager.enable = true;
+    git.enable = true;
+  };
+
+  home = {
+    username = lib.mkDefault "velen2077";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "24.11";
+    sessionPath = ["$HOME/.local/bin"];
+    sessionVariables = {
+      FLAKE = "$HOME/Development/megadots";
+    };
+  };
+}
