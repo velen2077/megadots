27ecd16ac2ecac21579c7155a49a6d756eca5e0f
velen2077 <velen2077@proton.me>
2025-04-23 19:53:57 +0100

    Added firefox hm module.

--- home/velen2077/endgame.nix
+++ home/velen2077/endgame.nix
@@ -3,5 +3,6 @@
     ./global
     ./features/development/vscodium
     ./features/theme/stylix
+    ./features/web/firefox
   ];
 }
--- hosts/config/optional/theme/default.nix
+++ hosts/config/optional/theme/default.nix
@@ -5,6 +5,8 @@
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
   stylix.image = ../../../../assets/wallpaper/cyber.jpg;
   stylix.imageScalingMode = "fill";
-  stylix.targets.gnome.enable = true;
-  stylix.targets.gtk.enable = false;
+  stylix.targets.gnome.enable = false;
+  stylix.targets.gtk.enable = true;
+  stylix.targets.plymouth.enable = true;
+  stylix.targets.console.enable = true;
 }
