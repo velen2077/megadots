32ab790f0a4d7d63e63c596dbe080fe102a1f13e
velen2077 <velen2077@proton.me>
2025-05-27 10:44:32 +0100

    Updated home configs based on misterio77's structure.

--- home/velen2077/global/default.nix
+++ home/velen2077/global/default.nix
@@ -8,8 +8,11 @@
 }: {
   imports =
     [
+      inputs.impermanence.nixosModules.home-manager.impermanence
+      ../features/cli
     ]
     ++ (builtins.attrValues outputs.homeManagerModules);
+
   nix = {
     package = lib.mkDefault pkgs.nix;
     settings = {
@@ -32,11 +35,26 @@
   home = {
     username = lib.mkDefault "velen2077";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "25.05";
     sessionPath = ["$HOME/.local/bin"];
     sessionVariables = {
-      FLAKE = "$HOME/Development/megadots";
+      NH_FLAKE = "$HOME/Development/megadots";
+    };
+
+    persistence = {
+      "/persist/${config.home.homeDirectory}" = {
+        defaultDirectoryMethod = "symlink";
+        directories = [
+          "Development"
+          "Documents"
+          "Downloads"
+          "Pictures"
+          "Videos"
+          ".local/bin"
+          ".local/share/nix" # trusted settings and repl history
+        ];
+        allowOther = true;
+      };
     };
   };
-
-  home.stateVersion = "24.11";
 }
