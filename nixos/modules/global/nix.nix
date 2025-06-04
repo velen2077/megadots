664a0b4f662a8c35029ad1ee38dbad53f7db1f37
velen2077 <velen2077@proton.me>
2025-06-04 09:56:53 +0000

    (update) Added cachyos module.

--- nixos/modules/global/nix.nix
+++ nixos/modules/global/nix.nix
@@ -1,11 +1,48 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.chaotic.nixosModules.default
     inputs.disko.nixosModules.disko
     inputs.impermanence.nixosModules.impermanence
   ];
-  # Mark admins as trusted users.
-  nix.settings.trusted-users = [
-    "@wheel"
-  ];
+  nix = {
+    settings = {
+      trusted-users = [
+        "@wheel"
+      ];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      warn-dirty = false;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Delete generations that haven't been activated in
+      # over 30 days.
+      options = "--delete-older-than +30";
+    };
+  };
+
+  # Enable passwordless sudo for members
+  # of wheel group.
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
+        ];
+        groups = ["wheel"];
+      }
+    ];
+  };
 }
--- nixos/systems/nixvm/configuration.nix
+++ nixos/systems/nixvm/configuration.nix
@@ -56,25 +56,6 @@
     };
   };
 
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
-    };
-    # Opinionated: disable channels
-    channel.enable = false;
-
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
-
   # FIXME: Add the rest of your current configuration
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
